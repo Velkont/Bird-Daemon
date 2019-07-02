22edbaedeb67f18460d76415e9283c41b4308303
Velkont <neon0000000@gmail.com>
2019-07-02 18:49:33 +0500

    Работающая версия

--- bird.py
+++ bird.py
@@ -4,36 +4,30 @@ path="/var/run/bird/bird.ctl"
 class Bird:
     def __init__(self, path):
         self.path = path
+        self.sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
+        self.sock.connect(self.path)
+        data=self.sock.recv(1024)
+        print(data.decode("utf-8"))
     def showStatus(self):
-        sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
-        sock.connect(self.path)
-        sock.send("birdc".encode('utf-8'))
-        sock.send("show status".encode('utf-8'))
-        print("ОТПРАВЛЕНО")
-        data=sock.recv(1024)
-        print(data)
-        sock.close()
+        self.sock.send(b"show status\n")
+        data=self.sock.recv(1024)
+        print(data.decode("utf-8"))
     def showInterfaces(self):
-        sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
-        sock.connect(self.path)
-        sock.send("show interfaces".encode('utf-8'))
-        print("ОТПРАВЛЕНО")
-        data=sock.recv(1024)
-        print(data)
-        sock.close()
+        self.sock.send(b"show interfaces\n")
+        data=self.sock.recv(1024)
+        print(data.decode("utf-8"))
     def showNeighbors(self):
-        sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
-        sock.connect(self.path)
-        sock.send("show ospf neighbors".encode('utf-8'))
-        print("ОТПРАВЛЕНО")
-        data=sock.recv(1024)
-        print(data)
-        sock.close()
+        self.sock.send(b"show ospf\n")
+        data=self.sock.recv(1024)
+        print(data.decode("utf-8"))
+    def __del__(self):
+        self.sock.close()
         
 new_bird=Bird(path)
 new_bird.showStatus()
 new_bird.showInterfaces()
 new_bird.showNeighbors()
+del new_bird
  
 
     
