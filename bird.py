29615651670c2d702b5d437231779375e0df4363
Velkont <neon0000000@gmail.com>
2019-07-05 19:01:07 +0500

    Подправил методы и протестил на стэнде

--- bird.py
+++ bird.py
@@ -1,111 +1,94 @@
 import os
 import socket
-path="/var/run/bird/bird.ctl"
+path = "/var/lib/docker-extvols/balancer-bird/run/bird.ctl"
 class Bird:
     def __init__(self, path):
         self.path = path
         self.sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
         self.sock.connect(self.path)
-        data=self.sock.recv(1024).replace(b".",b"").split()
+        data = self.sock.recv(1024).replace(b".", b"").split()
         if b"BIRD" not in data and b"ready" not in data:
-                print("Something went wrong")
-        if b"BIRD" in data and b"ready" in data:
-                print("OK")
+        	raise Exception("Socket is not correct")
+
+    def parseTable(self, data, keys):
+        k = []
+        dataToReturn = []
+        for i in data:
+        	k.append(i.split())
+        for index_i, i in enumerate(k):
+            dataToReturn.append({})
+            n = 0
+            for index_j, j in enumerate(k[index_i]):
+            	dataToReturn[index_i].update({keys[index_j]: k[index_i][index_j]})
+            	n += 1
+        return dataToReturn
+
     def showStatus(self):
         self.sock.send(b"show status\n")
         data=self.sock.recv(1024).decode("utf-8").split("\n")
-        keys=["Version","Router ID","Current server time","Last reboot","Last reconfiguration","Status"]
-        dataToReturn={}
-        for i in range(len(data)):
-        	if data[i].find("BIRD")!=-1:
-        		dataToReturn.update({keys[i]:data[i]})
-        	if data[i].find(" is ")!=-1:
-        		dataToReturn.update({keys[i]:data[i].split(" is ")[1]})
-        	if data[i].find(" on ")!=-1:
-        		dataToReturn.update({keys[i]:data[i].split(" on ")[1]})
-        	if data[i].find("Daemon")!=-1:
-        		dataToReturn.update({keys[i]:data[i][5:]})
-
-        return dataToReturn
-        	
+        keys=[
+            "Version", "Router ID", "Current server time",
+            "Last reboot", "Last reconfiguration", "Status"
+            ]
+        dataToReturn = {}
+        for index, i in enumerate(data):
+        	if data[index].find("BIRD") != -1:
+        		dataToReturn.update({keys[index]: data[index][5:]})
+        	if data[index].find(" is ") != -1:
+        		dataToReturn.update({keys[index]: data[index].split(" is ")[1]})
+        	if data[index].find(" on ") != -1:
+        		dataToReturn.update({keys[index]: data[index].split(" on ")[1]})
+        	if data[index].find("Daemon") != -1:
+        		dataToReturn.update({keys[index]: data[index][5:]})
+        return dataToReturn   	
         
     def showInterfaces(self):
         self.sock.send(b"show interfaces\n")
-        data=self.sock.recv(1024).decode("utf-8").split("\n")
-        del data[len(data)-1],data[len(data)-1]
-        dataToReturn={}
-        keys=[x[5:] for x in data if x.find("index=")!=-1]
-        n=[]
-        j=-1
-        for i in range(len(data)):
-        	if data[i].find("index=")==-1:
-        		n.append(data[i][6:])
-        	if data[i].find("index=")!=-1 or i==len(data)-1:
-        		if j>-1:
-        			dataToReturn.update({keys[j]:n})
-        			n=[]
-        		j+=1
+        data = self.sock.recv(1024).decode("utf-8").split("\n")[:-2]
+        dataToReturn = {}
+        keys = [x[5:] for x in data if x.find("index=") != -1]
+        n = []
+        j = -1
+        for index, i in enumerate(data):
+        	if data[index].find("index=") == -1:
+        		n.append(data[index][6:])
+        	if data[index].find("index=") != -1 or i == len(data) - 1:
+        		if j > -1:
+        			dataToReturn.update({keys[j]: n})
+        			n = []
+        		j += 1
         return dataToReturn
                 
-    def showNeighbors(self,*name):
-        if len(name)>0:
-                self.sock.send(b"show ospf "+ name[0].encode("utf-8") +b"\n")
-                data=self.sock.recv(1024).decode("utf-8")
-                if data[10:-1]=="Not a protocol":
-                        return None
-                else:
-                        data=data.split("\n")
-                        """Вообще не уверен, что этот блок хоть как-то работает, т.к. потестить на на чем, поэтому сделал по подобию с методом showProtocols, который у меня работает."""
-                        del data[0],data[1]
-                        k=[]
-                        for i in data:
-                                k.append(i.split())
-                        keys=["Router ID","Pri","State","DTime","Interface","Router IP"]
-                        dataToReturn=[]
-                        for i in range(len(k)):
-                                dataToReturn.append({})
-                                n=0
-                                for j in range(len(k[i])):
-                                        dataToReturn[i].update({keys[n]:k[i][j]})
-                                        n+=1
-                        return dataToReturn
-
-        else:
-                self.sock.send(b"show ospf\n")
-                data=self.sock.recv(1024).decode("utf-8")
-                data=data[5:]
-                if data=="There are multiple protocols running\n" or data=="There is no OSPF protocol running\n":
-                        return None
-  
-
+    def showNeighbors(self, name="", *args):
+        self.sock.send(b"show ospf neighbors "
+                       +name.encode("utf-8") 
+                       +b"\n")
+        data = self.sock.recv(1024).decode("utf-8")
+        if data[:4] == "9001":
+            return None
+        data = data.split("\n")[2:-2]
+        keys = ["Router ID", "Pri", "State", "DTime", "Interface", "Router IP"]
+        dataToReturn = self.parseTable(data, keys)
+        return dataToReturn
                         
     def showProtocols(self):
         self.sock.send(b"show protocols\n")
-        data=self.sock.recv(1024).decode("utf-8").split("\n")
-        del data[0],data[len(data)-1],data[len(data)-1]
-        k=[]
-        for i in data:
-                k.append(i.split())
-        keys=["Name","Proto","Table","State","Since","Info"]
-        dataToReturn=[]
-        for i in range(len(k)):
-                dataToReturn.append({})
-                n=0
-                for j in range(len(k[i])):
-                        dataToReturn[i].update({keys[n]:k[i][j]})
-                        n+=1
+        data = self.sock.recv(1024).decode("utf-8").split("\n")
+        data = data[1:-2]
+        data[0] = data[0][5:]
+        keys = ["Name", "Proto", "Table", "State", "Since", "Info"]
+        dataToReturn = self.parseTable(data, keys)
         return dataToReturn
 
              
     def __del__(self):
         self.sock.close()
         
-new_bird=Bird(path)
-new_bird.showStatus()
-new_bird.showInterfaces()
-new_bird.showNeighbors()
-new_bird.showProtocols()
-del new_bird
+new_bird = Bird(path)
+print(new_bird.showStatus())
+print(new_bird.showInterfaces())
+print(new_bird.showNeighbors("External"))
+print(new_bird.showProtocols())
  
 
     
