3bb32e2ec55455b6b052cfd8d9b7336ca596bf0e
Velkont <neon0000000@gmail.com>
2019-07-17 22:15:55 +0500

    Конфиг + начал async

--- bird.py
+++ bird.py
@@ -1,27 +1,66 @@
-import os
 import socket
 import time
 import logging
+import yaml
+import os
+import re
+import asyncio
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.ERROR)
-path = "/var/lib/docker-extvols/balancer-bird/run/bird.ctl"
+
+
+class Config:
+    def __init__(self):
+        self.dir_name = os.path.dirname(__file__)
+        self.config_file_name = os.path.join(self.dir_name, './config.yaml')
+
+    def load_config(self):
+        with open(self.config_file_name, "r") as config_file:
+            config = yaml.load(config_file)
+        return config
+
+    def get_methods_config(self):
+        config = self.load_config()
+        methods = []
+        for method in config["methods"]:
+            methods.append(config["methods"][method])
+        dic_with_data_functions = {
+            "Status": new_bird.show_status,
+            "Protocols": new_bird.show_protocols,
+            "Neighbors": new_bird.show_neighbors
+        }
+        for dic in methods:
+            if dic["name"] == "Neighbors":
+                data = dic_with_data_functions[dic["name"]](dic["neighbor"])
+                dic.update({"data": data})
+            else:
+                data = dic_with_data_functions[dic["name"]]()
+                dic.update({"data": data})
+        return methods
+
+    def get_path(self):
+        config = self.load_config()
+        path = config["paths"]["path_to_bird"]
+        return path
+
 
 class Bird:
     def __init__(self, path):
         self.path = path
-        self.sock = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.sock.connect(self.path)
         data = self.sock.recv(1024).replace(b".", b"").split()
         if b"BIRD" not in data and b"ready" not in data:
-        	raise Exception("Socket is not correct")
-        	
+            raise Exception("Socket is not correct")
+
     def get_data(self, command):
         self.sock.send(("{0}{1}".format(command, "\n")).encode("utf-8"))
         data = self.sock.recv(1024)
+        logging.debug(data)
         logging.debug("get_data end")
         data = data.decode("utf-8")
         return data
-        
+
     def parse_table(self, data, keys):
         k = []
         data_to_return = []
@@ -36,7 +75,6 @@ class Bird:
         logging.debug(data_to_return)
         logging.debug("--------------")
         return data_to_return
-        
 
     def show_status(self):
         data = self.get_data("show status")
@@ -51,23 +89,25 @@ class Bird:
         data_to_return = []
         dic = {}
         for i, key in zip(data, keys):
-        	if i.find("BIRD") != -1:
-        		dic.update({key: i[5:]})
-        	if i.find(" is ") != -1:
-        		dic.update({key: i.split(" is ")[1]})
-        	if i.find(" on ") != -1:
-        		dic.update({key: i.split(" on ")[1]})
-        	if i.find("Daemon") != -1:
-        	    if i.find("Daemon is up and running") != -1:
-        	        dic.update({key: 1})
-        	    else:
-        	        dic.update({key: 0})
+            if re.findall(r'1000-?', i):
+                dic.update({key: re.sub(r'1000-?', '', i)})
+            elif re.findall(r'0013-?', i):
+                if re.findall(r'Daemon is up and running', i):
+                    dic.update({key: 1})
+                else:
+                    dic.update({key: 0})
+            elif re.findall(r'1011-?', i):
+                ip = re.findall(r'\d+.\d+.\d+.\d+', i)[0]
+                dic.update({key: ip})
+            else:
+                date = re.findall(r'\d+-\d+-\d+ \d+:\d+:\d+.\d+', i)[0]
+                dic.update({key: date})
         data_to_return.append(dic)
         logging.debug("show_status end")
         logging.debug(data_to_return)
         logging.debug("--------------")
-        return data_to_return   	
-        
+        return data_to_return
+
     def show_interfaces(self):
         data = self.get_data("show interfaces")
         data = data.split("\n")[:-2]
@@ -77,29 +117,29 @@ class Bird:
         n = []
         j = -1
         for i, key in zip(data, keys):
-        	if i.find("index=") == -1:
-        		n.append(i[6:])
-        	if i.find("index=") != -1 or i == len(data) - 1:
-        		if j > -1:
-        			data_to_return.update({key: n})
-        			n = []
-        		j += 1
+            if i.find("index=") == -1:
+                n.append(i[6:])
+            if i.find("index=") != -1 or i == len(data) - 1:
+                if j > -1:
+                    data_to_return.update({key: n})
+                    n = []
+                j += 1
         data_to_return.append(dic)
         logging.debug("show_status end")
         logging.debug("--------------")
         return data_to_return
-                
+
     def show_neighbors(self, name="", *args):
         data = self.get_data("show ospf neighbors {0}".format(name))
         if data.startswith("9001"):
             return None
-        data = data.split("\n")[2:-2]
+        data = data.split('\n')[2:-2]
         keys = ["Router ID", "Pri", "State", "DTime", "Interface", "Router IP"]
         data_to_return = self.parse_table(data, keys)
         logging.debug("show_neighbors end")
         logging.debug("--------------")
         return data_to_return
-                        
+
     def show_protocols(self):
         data = self.get_data("show protocols")
         data = data.split("\n")
@@ -110,10 +150,11 @@ class Bird:
         logging.debug("show_protocols end")
         logging.debug("--------------")
         return data_to_return
-             
+
     def __del__(self):
         self.sock.close()
-        
+
+
 class FormatData:
     @staticmethod
     def convert_bird_table(table, name, ID, param):
@@ -121,81 +162,96 @@ class FormatData:
         try:
             for dic in table:
                 if ID in dic.values() and param in dic:
-                    data_to_return.append("{0}.{1}.{2} {3} {4}".format(str(name), str(ID), 
-                                                                       str(param), str(dic[param]),
-                                                                       str(int(time.time()))))
+                    metric = "{0}.{1}.{2} {3} {4}".format(
+                        str(name), str(ID),
+                        str(param), str(dic[param]),
+                        str(int(time.time())))
+                    data_to_return.append(metric)
                 elif param in dic and ID is None:
-                    data_to_return.append("{0}.{1} {2} {3}".format(str(name), str(param), 
-                                                                   str(dic[param]), str(int(time.time()))))
+                    metric = "{0}.{1} {2} {3}".format(
+                        str(name), str(param),
+                        str(dic[param]),
+                        str(int(time.time())))
+                    data_to_return.append(metric)
                 elif ID in dic.values() and param is None:
                     for key in dic:
-	                    data_to_return.append("{0}.{1}.{2} {3} {4}".format(str(name), str(ID),
-	                                                                       str(key), str(dic[key]), 
-	                                                                       str(int(time.time()))))
+                        metric = "{0}.{1}.{2} {3} {4}".format(
+                            str(name), str(ID),
+                            str(key), str(dic[key]),
+                            str(int(time.time())))
+                        data_to_return.append(metric)
                 elif "Version" in dic and param is None:
-                        for key in dic:
-                            data_to_return.append("{0}.{1} {2} {3}".format(str(name), str(key),
-                                                                           str(dic[key]), 
-                                                                           str(int(time.time()))))
-	                
+                    for key in dic:
+                        metric = "{0}.{1} {2} {3}".format(
+                            str(name), str(key),
+                            str(dic[key]),
+                            str(int(time.time())))
+                        data_to_return.append(metric)
+
                 elif ID is None and param is None:
-	                key_ID = None
-	                for key in dic:
-	                    if key_ID is None:
-	                        key_ID = key
-	                    data_to_return.append("{0}.{1}.{2} {3} {4}".format(str(name), str(dic[key_ID]), 
-	                                                                       str(key), str(dic[key]), 
-	                                                                       str(int(time.time()))))
+                    key_ID = None
+                    for key in dic:
+                        if key_ID is None:
+                            key_ID = key
+                        metric = "{0}.{1}.{2} {3} {4}".format(
+                            str(name), str(dic[key_ID]),
+                            str(key), str(dic[key]),
+                            str(int(time.time())))
+                        data_to_return.append(metric)
             if not data_to_return:
                 data_to_return.append("None")
             return data_to_return
         except Exception as error:
-	        logging.exception("error while converting table")
-	        logging.exception(error)
-	        logging.debug("--------------")
-           
-	            
-    @staticmethod          
-    def convert_from_bird_to_metrics(data, name, ID=None, param=None, **kwargs):
-        if data == None:
-	        return ["None"]
+            logging.exception("error while converting table")
+            logging.exception(error)
+            logging.debug("--------------")
+
+    @staticmethod
+    def convert_from_bird_to_metrics(data, name, ID=None,
+                                     param=None, **kwargs):
+        if data is None:
+            return ["None"]
         else:
             return FormatData.convert_bird_table(data, name, ID, param)
 
+
 class Metrics:
-	def __init__(self):
-	    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	    self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	    self.sock.connect(("", 2003))
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.connect(("", 2003))
 
-	def send_metrics(self, data):
-	    logging.debug("sen_metrics start")
-	    logging.debug(data)
-	    logging.debug("--------------")
-	    try:    
-	        for i in data:
-	            self.sock.send(i.encode("utf-8"))
+    def send_metrics(self, data):
+        logging.debug("sen_metrics start")
+        logging.debug(data)
+        logging.debug("--------------")
+        try:
+            for i in data:
+                self.sock.send(i.encode("utf-8"))
+        except Exception as error:
+            logging.exception("Sending error")
+            logging.exception(error)
+            logging.debug("--------------")
 
-	    except Exception as error:
-	        logging.exception("Sending error")
-	        logging.exception(error)
-	        logging.debug("--------------")
+    def __del__(self):
+        self.sock.close()
 
-	def __del__(self):
-	    self.sock.close()
 
-		
 if __name__ == "__main__":
+    new_config = Config()
     new_metrics = Metrics()
-    new_bird = Bird(path)
-    config = [
-        {"data": new_bird.show_status(), "name": "Status"},
-        {"data": new_bird.show_protocols(), "name": "Protocols", "ID": "Internal"},
-        {"data": new_bird.show_neighbors(), "name": "Neighbors"},
-    ]
-    while 1:
-        for i in config:
-            data_to_send = FormatData.convert_from_bird_to_metrics(**i)
-            new_metrics.send_metrics(data_to_send)
-        time.sleep(20)
-        
+    new_bird = Bird(new_config.get_path())
+    methods_config = new_config.get_methods_config()
+    loop = asyncio.get_event_loop()
+
+    async def main_coroutine():
+        while True:
+            for i in methods_config:
+                data_to_send = FormatData.convert_from_bird_to_metrics(**i)
+                new_metrics.send_metrics(data_to_send)
+            await asyncio.sleep(20)
+    try:
+        asyncio.ensure_future(main_coroutine())
+        loop.run_forever()
+    finally:
+        loop.close()
--- tests.py
+++ tests.py
@@ -1,50 +1,56 @@
 import unittest
 import bird
 import time
+
+
 class TestBird(bird.Bird):
     def __init__(self):
         pass
-        
+
     def get_data(self, name):
         return self.data
-        
+
     def __del__(self):
         pass
-        
-class TestBirdClass(unittest.TestCase):  
+
+
+class TestBirdClass(unittest.TestCase):
 
     def setUp(self):
         self.test_bird = TestBird()
-        
+
     def test_output_show_neighbors(self):
-        self.test_bird.data = b'1013-Internal:\n Router ID   \tPri\t     State     \tDTime\tInterface  Router IP   \n 172.20.30.1\t  1\tFull/PtP  \t 31.317\tveth2internal 172.20.30.1    \n0000 \n'.decode("utf-8")
-        end_data= [{
-            'Router ID': '172.20.30.1', 
-            'Pri': '1', 
-            'State': 'Full/PtP', 
-            'DTime': '31.317', 
-            'Interface': 'veth2internal', 
+        self.test_bird.data = b'''1013-Internal:\n Router ID   \tPri\t State     \tDTime\tInterface  Router IP \n 172.20.30.1\t  1\tFull/PtP \t 31.317\tveth2internal 172.20.30.1 \n0000 \n'''.decode("utf-8")
+        end_data = [{
+            'Router ID': '172.20.30.1',
+            'Pri': '1',
+            'State': 'Full/PtP',
+            'DTime': '31.317',
+            'Interface': 'veth2internal',
             'Router IP': '172.20.30.1'
         }]
         self.assertEqual(self.test_bird.show_neighbors("Internal"), end_data)
-        
+
     def test_output_show_neighbors_none(self):
-        self.test_bird.data = '9001 There are multiple OSPF protocols running\n'
+        self.test_bird.data = '''9001 There are
+                              multiple OSPF protocols running\n'''
         self.assertEqual(None, self.test_bird.show_neighbors())
-        
+
     def test_parse_table(self):
-        self.test_bird.data = [' 172.20.30.1\t  1\tFull/PtP  \t 33.217\tveth2internal 172.20.30.1    ']
+        self.test_bird.data = [''' 172.20.30.1\t  1\tFull/PtP
+                               \t 33.217\tveth2internal 172.20.30.1    ''']
         end_data = [{
-            'Router ID': '172.20.30.1', 
-            'Pri': '1', 
-            'State': 'Full/PtP', 
-            'DTime': '33.217', 
-            'Interface': 'veth2internal', 
+            'Router ID': '172.20.30.1',
+            'Pri': '1',
+            'State': 'Full/PtP',
+            'DTime': '33.217',
+            'Interface': 'veth2internal',
             'Router IP': '172.20.30.1'
         }]
         keys = ["Router ID", "Pri", "State", "DTime", "Interface", "Router IP"]
-        self.assertEqual(self.test_bird.parse_table(self.test_bird.data, keys), end_data)
-        
+        self.assertEqual(end_data,
+                         self.test_bird.parse_table(self.test_bird.data, keys))
+
     def test_show_status(self):
         self.test_bird.data = """1000-BIRD 2.0.0
                                  1011-Router ID is 172.20.20.2
@@ -53,65 +59,78 @@ class TestBirdClass(unittest.TestCase):
                                  Last reconfiguration on 2019-07-15 05:42:51.595
                                  0013 Daemon is up and running"""
         end_data = [{
-            'Version': 'BIRD 2.0.0', 
-            'Router ID': '172.20.20.2', 
-            'Current server time': '2019-07-15 07:57:33.909', 
-            'Last reboot': '2019-07-15 05:42:51.595', 
-            'Last reconfiguration': '2019-07-15 05:42:51.595', 
+            'Version': 'BIRD 2.0.0',
+            'Router ID': '172.20.20.2',
+            'Current server time': '2019-07-15 07:57:33.909',
+            'Last reboot': '2019-07-15 05:42:51.595',
+            'Last reconfiguration': '2019-07-15 05:42:51.595',
             'Status': 1
         }]
         self.assertEqual(self.test_bird.show_status(), end_data)
-           
+
+
 class TestFormatClass(unittest.TestCase):
     def setUp(self):
         self.test_dic = [
-            {'Name': 'direct1', 'Proto': 'Direct', 'Table': '---', 'State': 'up', 'Since': '17:26:29.843'}, 
-            {'Name': 'kernel1', 'Proto': 'Kernel', 'Table': 'master4', 'State': 'up', 'Since': '17:26:29.843'}, 
-            {'Name': 'device1', 'Proto': 'Device', 'Table': '---', 'State': 'up', 'Since': '17:26:29.843'}, 
-            {'Name': 'static1', 'Proto': 'Static', 'Table': 'master4', 'State': 'up', 'Since': '17:26:29.843'}, 
-            {'Name': 'External', 'Proto': 'OSPF', 'Table': 'master4', 'State': 'up', 'Since': '17:26:29.843', 'Info': 'Running'},
-            {'Name': 'Internal', 'Proto': 'OSPF', 'Table': 'master4', 'State': 'up', 'Since': '17:26:29.843', 'Info': 'Running'}
+            {'Name': 'direct1', 'Proto': 'Direct', 'Table': '---',
+             'State': 'up', 'Since': '17:26:29.843'},
+            {'Name': 'kernel1', 'Proto': 'Kernel', 'Table': 'master4',
+             'State': 'up', 'Since': '17:26:29.843'},
+            {'Name': 'device1', 'Proto': 'Device', 'Table': '---',
+             'State': 'up', 'Since': '17:26:29.843'},
+            {'Name': 'static1', 'Proto': 'Static', 'Table': 'master4',
+             'State': 'up', 'Since': '17:26:29.843'},
+            {'Name': 'External', 'Proto': 'OSPF', 'Table': 'master4',
+             'State': 'up', 'Since': '17:26:29.843', 'Info': 'Running'},
+            {'Name': 'Internal', 'Proto': 'OSPF', 'Table': 'master4',
+             'State': 'up', 'Since': '17:26:29.843', 'Info': 'Running'}
         ]
         self.test_line = [{
             'Router ID': '172.20.30.1',
-            'Pri': '1', 
-            'State': 'Full/PtP', 
-            'DTime': '37.453', 
-            'Interface': 'veth2internal', 
+            'Pri': '1',
+            'State': 'Full/PtP',
+            'DTime': '37.453',
+            'Interface': 'veth2internal',
             'Router IP': '172.20.30.1'
         }]
-        
+
     def test_output_from_bird_to_metrics(self):
         start_data = self.test_line
         end_data = ["Neighbors.172.20.30.1.Pri 1 " + str(int(time.time()))]
-        self.assertEqual(bird.FormatData.convert_from_bird_to_metrics(start_data, name="Neighbors", 
-                                                                      ID='172.20.30.1', param="Pri"), end_data)
-        
-    def test_output_convert_bird_table(self):
+        self.assertEqual(
+            end_data,
+            bird.FormatData.convert_from_bird_to_metrics(start_data,
+                                                         name="Neighbors",
+                                                         ID='172.20.30.1',
+                                                         param="Pri"))
+
+    def test_output_convert_bird_table_neighbors(self):
         start_data = self.test_line
         end_data = ["Neighbors.172.20.30.1.Pri 1 " + str(int(time.time()))]
-        self.assertEqual(bird.FormatData.convert_bird_table(start_data, name="Neighbors", 
-                                                            ID='172.20.30.1', param="Pri"), end_data)
-    def test_output_convert_bird_table(self):
+        self.assertEqual(end_data,
+                         bird.FormatData.convert_bird_table(start_data,
+                                                            name="Neighbors",
+                                                            ID='172.20.30.1',
+                                                            param="Pri"))
+
+    def test_output_convert_bird_table_protocols(self):
         start_data = self.test_dic
         data = [
-            'Protocols.Internal.Name Internal', 
-            'Protocols.Internal.Proto OSPF', 
-            'Protocols.Internal.Table master4', 
-            'Protocols.Internal.State up', 
+            'Protocols.Internal.Name Internal',
+            'Protocols.Internal.Proto OSPF',
+            'Protocols.Internal.Table master4',
+            'Protocols.Internal.State up',
         ]
         end_data = []
         for i in data:
             end_data.append('{0} {1}'.format(i, str(int(time.time()))))
 
-        self.assertEqual(bird.FormatData.convert_bird_table(start_data, name="Protocols", 
-                                                            ID='Internal', param=None)[0:4], end_data)
-    def test_convert_bird_table_attribute_error(self):
-        self.assertRaises(AttributeError, bird.FormatData.convert_bird_table(self.test_bird,show_neighbors(), name="Neighbors", 
-                                                                            ID='Internal', param=None))
-            
-        
+        self.assertEqual(end_data,
+                         bird.FormatData.convert_bird_table(start_data,
+                                                            name="Protocols",
+                                                            ID='Internal',
+                                                            param=None)[0:4])
 
-        
+
 if __name__ == "__main__":
     unittest.main()
